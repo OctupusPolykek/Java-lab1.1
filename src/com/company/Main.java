e13083721d968088a7eda9c8249adfae4ce5ed50
Misha Sokil <rasiapypkin2014@gmail.com>
2021-10-25 10:21:28 +0300

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        WareHouse wareHouse = new WareHouse();
+
+        System.out.print("Enter the number of employees: ");
+        int length = scanner.nextInt();
+        System.out.println("========");
+        for (int i = 0; i < length; i++) {
+            System.out.println("Product  №" + (i + 1) + ":");
+            System.out.print("Amount: ");
+            int amount = scanner.nextInt();
+            System.out.print("Price: ");
+            int price = scanner.nextInt();
+            System.out.print("Year: ");
+            int year = scanner.nextInt();
+            System.out.print("Manufacturer: ");
+            String manufacturer = scanner.next();
+            System.out.println("========");
+
+            wareHouse.addProduct(new Product(amount, price, year, manufacturer));
+        }
+
+        WareHouse.getDearestProduct(wareHouse);
+    }
+}
---
+++ src/com/company/Product.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Product {
+    private final int amount;
+    private final int price;
+    private final int year;
+    private final String manufacturer;
+
+    public Product(int amount, int price, int year, String manufacturer) {
+        if (amount > 0)
+            this.amount = amount;
+        else
+            throw new Error("Amount must be > 0.");
+        if (price > 0)
+            this.price = price;
+        else
+            throw new Error("Price must be > 0.");
+        if (year > 0)
+            this.year = year;
+        else
+            throw new Error("Year must be > 0.");
+        if (manufacturer != null && !manufacturer.isEmpty())
+            this.manufacturer = manufacturer;
+        else
+            throw new Error("Manufacturer cannot be empty.");
+    }
+    //Getters
+    public int getAmount() {
+        return amount;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public int getYear() {
+        return year;
+    }
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    //Methods
+    public void printInfo() {
+        System.out.println("Amount: " + this.getAmount());
+        System.out.println("Price: " + this.getPrice());
+        System.out.println("Year: " + this.getYear());
+        System.out.println("Manufacturer: " + this.getManufacturer());
+    }
+}
